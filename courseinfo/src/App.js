ab9505254c2bba4e21746d896a4e6a900ed6bf09
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-05-03 16:31:36 +0200

    2.4_course_contents_step9

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -11,7 +11,7 @@ const Total = ({ modules }) => {
     return sum + module.exercises
   }, 0)
   return(
-  <p>Number of exercises {numEx}</p>
+  <p><b>Number of exercises {numEx}</b></p>
   )
 }
 
@@ -43,38 +43,58 @@ const Course = ({ course }) => {
   )
 }
 
-
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+  const App = () => {
+    const courses = [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
       {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
       }
     ]
-  }
+  
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course =>
+        <Course course={course} />
+        )}
     </div>
   )
 }
