5436d9c0b00277914b7d0e0f7d822abaa4e0a4e1
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-05-09 20:15:58 +0200

    2.14_data_for_countries_step_3

--- data-for-countries/src/components/CountryDetail.js
+++ data-for-countries/src/components/CountryDetail.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import RelevantInfo from './RelevantInfo'
+import Weather from './Weather'
 
-const CountryDetail = ({ country }) => {
+const CountryDetail = ({ country, weather }) => {
   return (
     <div>
       <RelevantInfo country={country} />
+      <Weather country={country} weather={weather} />
     </div>
   )
 }
