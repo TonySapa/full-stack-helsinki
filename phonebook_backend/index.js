01ebdbbdcb936afd696d7f7c890e4e941e601e5a
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-05-21 15:41:23 +0200

    3.2_phonebook_backend_step2

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+const infoMessage = `<p>Phonebook has info for ${persons.length} people</p>${new Date()}`
+
+
+app.get('/info', (req, res) => {
+  res.send(infoMessage)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
