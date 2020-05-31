4d9b40a8163f0a5b73f446807895c1c60e382df2
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-05-31 16:16:40 +0200

    3.11_phonebook_full_stack

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -23,7 +23,7 @@ const App = (props) => {
   const hook = () => {
     console.log('effect')
     axios
-      .get('http://localhost:3001/api/persons')
+      .get('https://mighty-beach-17427.herokuapp.com/api/persons')
       .then(response => {
         console.log('promise fulfilled')
         setpersons(response.data)
