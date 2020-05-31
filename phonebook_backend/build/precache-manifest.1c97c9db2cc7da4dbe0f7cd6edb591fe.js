e0ae113bc7482771b0d96d174f6953bb7ce1ad47
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-05-31 16:23:58 +0200

    3.11_phonebook_full_stack

---
+++ phonebook_backend/build/precache-manifest.1c97c9db2cc7da4dbe0f7cd6edb591fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe0c0cd8dd6ac002fd51219f3c88fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b83929374ceb47b4df",
+    "url": "/static/js/main.60eedfeb.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
