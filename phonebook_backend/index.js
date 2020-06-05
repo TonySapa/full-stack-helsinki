4ab9693ea235e11637e7da6537b24bb22687a48d
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-05 16:57:37 +0200

    3.16_phonebook_database_step4

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -99,6 +99,25 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
 /*
 const infoMessage = `<p>Phonebook has info for ${persons.length} people</p>${new Date()}`
 
