7311b587604ebb6d88da2e6d757966aa8d139f52
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-10 14:38:53 +0200

    4.4_helper_functions_and_unit_tests_step2

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let likes = 0
+  blogs.map(blog => likes = likes + blog.likes)
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
