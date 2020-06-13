c63ac4a80b95de814d0580d17de19895faf5b8ca
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-13 17:18:08 +0200

    4.13_bloglist_expansions_step_1

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -86,6 +86,22 @@ test('If title or url is missing respond with 400', async () => {
   expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
 })
 
+test('Delete a single note', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length -1
+  )
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).not.toContain(blogToDelete.title)
+})
 
 afterAll(() => {
   mongoose.connection.close()
