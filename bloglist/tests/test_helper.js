591a4ce48a7c26ba88b6dd27553bd4b755775897
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-13 14:26:24 +0200

    4.8_blog_list_tests_step_1

---
+++ bloglist/tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Overrreacted',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/',
+    likes: 33
+  },
+  {
+    title: 'David Walsh Blog',
+    author: 'David Walsh',
+    url: 'https://davidwalsh.name/',
+    likes: 19
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ content: 'willremovethissoon' })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
