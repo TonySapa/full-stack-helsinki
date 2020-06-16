f3294b74d6c4a09191493cd7d8be06fab3276f93
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-16 15:29:57 +0200

    202006161529a

--- bloglist/App.js
+++ bloglist/App.js
@@ -1,14 +1,14 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blog')
+const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-var _ = require('lodash')
-
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -25,9 +25,9 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
-
+app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
