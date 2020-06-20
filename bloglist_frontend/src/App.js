c9ba60157fd399047329bedec3c8671705849cea
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-20 16:07:45 +0200

    5.6_bloglist_frontend_step_6

--- bloglist_frontend/src/App.js
+++ bloglist_frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 import InfoAlert from './components/InfoAlert'
 import DangerAlert from './components/DangerAlert'
 import blogService from './services/blogs'
@@ -7,16 +9,11 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([]) 
-  const [newTitle, setnewTitle] = useState('')
-  const [newAuthor, setnewAuthor] = useState('')
-  const [newUrl, setnewUrl] = useState('')
-  const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [infoMessage, setInfoMessage] = useState('')
-  const [dangerMessage, setDangerMessage] = useState('')
-
+  const [infoMessage, setInfoMessage] = useState(null)
+  const [dangerMessage, setDangerMessage] = useState(null)
 
   useEffect(() => {
     blogService
@@ -34,29 +31,6 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
-
-  const addBlog = (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl
-      // id: blogs.length + 1,
-    }
-  
-    blogService
-      .create(blogObject)
-      .then(returnedBlog => {
-        setBlogs(blogs.concat(returnedBlog))
-        setInfoMessage(`a new blog ${newTitle} added`)
-        setTimeout(() => {
-          setInfoMessage(null)
-        }, 5000)
-        setnewTitle('')
-        setnewAuthor('')
-        setnewUrl('')
-      })
-  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -88,16 +62,6 @@ const App = () => {
     setPassword('')
   }
 
-  const handleTitleChange = (event) => {
-    setnewTitle(event.target.value)
-  }
-  const handleAuthorChange = (event) => {
-    setnewAuthor(event.target.value)
-  }
-  const handleUrlChange = (event) => {
-    setnewUrl(event.target.value)
-  }
-
   const loginForm = () => (
     <div>
       <h1>log in to application</h1>
@@ -126,39 +90,28 @@ const App = () => {
     </div>    
   )
 
+  const addBlog = (blogObject) => {
+    blogFormRef.current.toggleVisibility()
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+      })
+  }
+
+
   const logOut = () => (
     <button onClick={handleLogout}>logout</button>
   )
 
+  const blogFormRef = React.createRef()
+
   const blogForm = () => (
-    <div>
-      <h1>create new</h1>
-      <form onSubmit={addBlog}>
-        <div>
-          title:
-          <input
-            value={newTitle}
-            onChange={handleTitleChange}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            value={newAuthor}
-            onChange={handleAuthorChange}
-          />
-        </div>
-        <div>
-          url:
-          <input
-            value={newUrl}
-            onChange={handleUrlChange}
-          />
-        </div>
-        <button type="submit">save</button>
-      </form>
-    </div>
+    <Togglable buttonLabel='new note' ref={blogFormRef}>
+      <BlogForm createBlog={addBlog} />
+    </Togglable>
   )
+
 
   return (
     <div>
