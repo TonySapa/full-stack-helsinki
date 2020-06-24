aa8c84fff10cde76fed186dd3f36848debaabd1c
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-24 14:27:56 +0200

    5.13_bloglist_tests_step_1

---
+++ bloglist_frontend/src/components/Blog.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+//import { prettyDOM } from '@testing-library/dom'
+import Blog from './Blog'
+
+test('renders title and author but not url neither likes', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Toni Sánchez'
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  expect(component.container).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
+})
