1e5fe456c4a1f22e994613b11d59316be6ea4977
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-06-26 12:44:56 +0200

    5.15_bloglist_step_3

--- bloglist_frontend/src/components/Blog.test.js
+++ bloglist_frontend/src/components/Blog.test.js
@@ -28,7 +28,8 @@ describe('<Togglable />', () => {
       user: {
         user: "myUser",
         username: "myusername"
-      }
+      },
+      id: 'myId'
     }
 
     const user = 'thisUser'
@@ -66,5 +67,47 @@ describe('<Togglable />', () => {
     const button = component.getByText('view')
     fireEvent.click(button)
     expect(div).toHaveStyle('display: block')
+  })
+
+  test('If like button is pressed twice addLike is called twice', () => {
+    const blog = {
+      title: "myTitle",
+      author: "myAuthor",
+      url: "myUrl",
+      likes: 47,
+      user: {
+        user: "myUser",
+        username: "myusername"
+      },
+      id: 'myId'
+    }
+
+    const updateBlog = () => {
+      console.log('updateBlog')
+    }
+
+    const blogObject = () => {
+      console.log('blogObject')
+    }
+
+
+    const addLike = () => {
+      console.log('addlike')
+    }
+    
+    const mockHandler = jest.fn()
+
+    const { getByText } = render(
+      <LikeButton
+        buttonLabel='like-xyz' blog={blog} updateBlog={mockHandler}
+        id={blog.id} blogObject={blogObject} addLike={addLike}
+      />
+    )
+    const button = getByText('like-xyz')
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  
   })
 })
