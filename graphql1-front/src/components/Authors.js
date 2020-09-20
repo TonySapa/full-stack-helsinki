5411899d4abccdf35c3293cb78089ff4c580c268
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-09-20 13:57:31 +0200

    8.11 Authors birth year

--- graphql1-front/src/components/Authors.js
+++ graphql1-front/src/components/Authors.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client'
+import React, { useState } from 'react'
+import { gql, useMutation, useQuery } from '@apollo/client'
 
 const ALL_PERSONS = gql`
   query {
@@ -11,8 +11,27 @@ const ALL_PERSONS = gql`
   }
 `
 
+const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $born: Int!) {
+    editAuthor(
+      name: $name,
+      born: $born
+    ) {
+      name,
+      born
+    }
+  }
+`
+
+
 const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
   const result = useQuery(ALL_PERSONS)
+  const [ updateAuthor ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ {query: ALL_PERSONS } ]
+  })
 
   if (!props.show) {
     return null
@@ -21,6 +40,15 @@ const Authors = (props) => {
   if ( result.loading ) {
     return <div>loading...</div>
   }
+
+
+  const submit = async (event) => {
+    event.preventDefault()
+    updateAuthor({ variables: { name, born } })
+    setName('')
+    setBorn('')
+  }
+
 
   return (
     <div>
@@ -45,6 +73,23 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
+
+      <h2>set birth year</h2>
+      <form onSubmit={submit}>
+        <table>
+          <tr>
+            <td>name</td>
+            <td><input value={name} onChange={({ target }) => setName(target.value)} /></td>
+          </tr>
+          <tr>
+            <td>born</td>
+            <td><input value={born} onChange={({ target }) => setBorn(Number(target.value))}/></td>
+          </tr>
+          <tr>
+            <td><button type='submit'>update author</button></td>
+          </tr>
+        </table>
+      </form>
 
     </div>
   )
